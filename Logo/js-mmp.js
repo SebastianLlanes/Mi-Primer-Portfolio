bc61040a40efd4dda7b610b8f1380a1d9ec29a72
Seba Llanes <sebastianllanessaavedra@gmail.com>
2022-11-10 20:04:04 -0300

    'Estilización general. Avances en js, falta resolver funcionalidad de los botones'

--- Logo/js-mmp.js
+++ Logo/js-mmp.js
@@ -1,16 +1,18 @@
 const htmlButton = document.querySelector('.html-button');
 const htmlDiploma = document.querySelector('.html-button_span');
+
+
 let contador = 0;
-
+   
 function showDiploma() {
-  if (contador == 0) {
-    htmlDiploma.classList.remove('inactive');
-    contador = 1;
-  } else {htmlDiploma.classList.add('inactive')};
-          contador = 0;
- }
-
-htmlButton.addEventListener('click', showDiploma, true)
-
-
+      if (contador == 0) {
+            htmlDiploma.classList.remove('inactive');
+            contador = 1;
+      } else {htmlDiploma.classList.add('inactive');
+              contador = 0;
+          }
+      }
+ 
+        
+  htmlButton.addEventListener('click', showDiploma, true)
 
