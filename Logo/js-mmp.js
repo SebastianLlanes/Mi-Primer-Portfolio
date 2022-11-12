6eadba5ba3f88dcda8568bdb75812f1b4b50858e
Seba Llanes <sebastianllanessaavedra@gmail.com>
2022-11-12 08:01:36 -0300

    'Más estilización. Agregado responsive design. FALTA RESOLVER JS'

--- Logo/js-mmp.js
+++ Logo/js-mmp.js
@@ -1,18 +1,33 @@
-const htmlButton = document.querySelector('.html-button');
-const htmlDiploma = document.querySelector('.html-button_span');
+ const htmlButton = document.querySelector('.html-button');
+ const htmlDiploma = document.querySelector('.html-button_span');
+ let contador = 0;
 
-
-let contador = 0;
-   
-function showDiploma() {
-      if (contador == 0) {
-            htmlDiploma.classList.remove('inactive');
-            contador = 1;
-      } else {htmlDiploma.classList.add('inactive');
-              contador = 0;
-          }
-      }
+  function showDiploma() {
+        if (contador == 0) {
+              htmlDiploma.classList.remove('inactive');
+              contador = 1;
+        } else {htmlDiploma.classList.add('inactive');
+                contador = 0;
+            }
+        }
  
-        
-  htmlButton.addEventListener('click', showDiploma, true)
 
+
+const html = document.getElementsByClassName('.diploma-html')
+const css = document.getElementsByClassName('.diploma-css')
+const git = document.getElementsByClassName('.diploma-git')
+const js = document.getElementsByClassName('.diploma-js')
+
+let diploma = [html, css, git, js]
+
+const htmllButton = document.getElementsByClassName('.primer-boton')
+const cssButton = document.getElementsByClassName('.segundo-boton')
+const gitButton = document.getElementsByClassName('.tercer-boton')
+const jsButton = document.getElementsByClassName('.cuarto-boton')
+
+let botones = [htmlButton, cssButton, gitButton, jsButton]
+
+
+htmlButton.addEventListener('click', showDiploma, true)
+
+ 
